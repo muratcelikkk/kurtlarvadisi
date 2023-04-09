e45841739e4d8b7e56ada687b8f495b9260cc6ca
murat <murat.tc2014@gmail.com>
2023-04-09 16:04:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Data from './Data.json';
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className='inputContainer'>
+        <input className='search' type="text" placeholder="Konsey Üyesi Giriniz..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+      </div>
+      <div className='dataContainer'>
+        {
+          Data
+          .filter((val) => val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || searchTerm === "")
+          .map((val) => {
+              return (
+                <div className="data" key={val.id}>
+
+                  <img src={val.image} alt="" />
+                  <h3>{val.title}</h3>
+                  <p>{val.Description}</p>
+
+                </div>
+              )
+            })
+        }
+      </div>
     </div>
   );
 }
